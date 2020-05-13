ef4f4bba5ec94a6bb00332981e71477997e61f70
Gregory Hoyle <Gregory.Hoyle@alchemysystems.com>
2020-05-13 17:34:31 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "hoyle-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://h92jyjar9d.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ltKmZBWoS",
+    APP_CLIENT_ID: "1efqquk636j0hqg0u3rg7hvlut",
+    IDENTITY_POOL_ID: "1efqquk636j0hqg0u3rg7hvlu"
+  }
+};
