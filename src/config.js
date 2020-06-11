8b6c5c78a9c47e0c98e3b304e574aad80223b7b9
Gregory Hoyle <Gregory.Hoyle@alchemysystems.com>
2020-06-11 06:03:15 -0500

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_GgWwfvGupUp1FlKSekMPbHnB00wu9MOz5c",
   s3: {
     REGION: "us-east-1",
     BUCKET: "hoyle-notes-app-uploads"
@@ -11,6 +13,6 @@ export default {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_ltKmZBWoS",
     APP_CLIENT_ID: "1efqquk636j0hqg0u3rg7hvlut",
-    IDENTITY_POOL_ID: "1efqquk636j0hqg0u3rg7hvlu"
+    IDENTITY_POOL_ID: "us-east-1:952e597d-495b-4c4e-9b9f-b24f8a9c4da7"
   }
 };
